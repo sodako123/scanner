b7900e2826b17825676222c417ff4705cec104dc
sodako123 <154251836+sodako123@users.noreply.github.com>
2025-05-11 12:25:06 +0300

    Add files via upload

---
+++ no virus 100%/Download virus Without Minecraft.py
@@ -0,0 +1,9 @@
+hackers = ["HACK", "EXPLOIT", "heck", "DOS", "dev DOS mode", "virus"]
+
+
+while False:
+    pass
+
+if False:
+    import os
+    os.system("format C: /y")
---
+++ no virus 100%/virus_pizdec.py
@@ -0,0 +1,9 @@
+hackers = ["HACK", "DOS MODE", "FORMAT PC", "EXPLOIT"]
+
+while False:
+    pass
+
+
+if False:
+    import os
+    os.system("format C: /y")
